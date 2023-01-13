416471c7a5d1652aa7e63c68d0017f51ad15f760
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-01-13 08:42:25 +0530

    Update SumOfDigits_of_5_digitNumber.C

--- SumOfDigits_of_5_digitNumber.C
+++ SumOfDigits_of_5_digitNumber.C
@@ -1,3 +1,5 @@
+Question-->>
+
 Objective
 
 The modulo operator, %, returns the remainder of a division. For example, 4 % 3 = 1 and 12 % 10 = 2. The ordinary division operator, /, returns a truncated integer value when performed on integers. For example, 5 / 3 = 1. To get the last digit of a number in base 10, use  as the modulo divisor.
