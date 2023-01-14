a32ec7de7afdb140e7880b503c6cdc60a2332743
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-01-14 09:29:07 +0530

    Update 1_DArray_in_C.C

--- 1_DArray_in_C.C
+++ 1_DArray_in_C.C
@@ -31,3 +31,28 @@ Sample Input 1
 Sample Output 1
 
 76
+  
+  
+  Solution-->>
+  
+  
+  #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int *val = malloc(n*sizeof(int));
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&val[i]);
+        sum+=val[i];
+    }
+    printf("%i",sum);
+    free(val);
+}
+           
+
