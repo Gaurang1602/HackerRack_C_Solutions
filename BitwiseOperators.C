4e4edc6f86e7b22ea75a5221f4cf2b79f69bac03
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-01-16 08:23:57 +0530

    Update BitwiseOperators.C

--- BitwiseOperators.C
+++ BitwiseOperators.C
@@ -75,3 +75,44 @@ The maximum possible value of  that is also  is , so we print  on third line.
   
  
   Solution-->>
+  
+  
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  //Write your code here.
+}
+
+int main() {
+    int n, k,i,j;
+    int maxand=0;
+    int maxor=0;
+    int maxxor=0;
+    scanf("%d %d",&n,&k);
+    for(i=1;i<=n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            if(((i&j)>maxand)&&((i&j)<k)){
+             maxand=(i&j);
+            }
+            if(((i|j)>maxor)&&((i|j)<k)){
+                maxor=(i|j);
+            }
+            if(((i^j)>maxxor)&&((i^j)<k)){
+            maxxor=(i^j);
+            }
+        }
+    } 
+printf("%d\n%d\n%d\n",maxand,maxor,maxxor);
+return 0;
+}
+  
+  
+ 
+    
